507d9ddee3f700e4fb4ca4e98a8070704db5f560
Imanol Marin <imamarin14@gmail.com>
2020-08-28 11:30:44 -0300

    Agregue un script js para que me envie el email que inserten

---
+++ portafolio.js
@@ -0,0 +1,8 @@
+var e = document.getElementById ("email");
+var enviar = document.getElementById("boton");
+enviar.addEventListener("click",consolear);
+function consolear ()
+{
+    email = (e.value);
+    console.log("Este es su email: " + email);
+}
